bb6183511d2590e13038c8793c83e1ef535b3d4c
devmdocarmo <devmdocarmo@gmail.com>
2022-02-15 22:54:20 -0300

    heroku db

--- config/database.js
+++ config/database.js
@@ -2,6 +2,17 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
+      host: env('DATABASE_HOST', 'ec2-54-157-15-228.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd9j00c2v7ah9cu'),
+      user: env('DATABASE_USERNAME', 'boaibsmriypgak'),
+      password: env('DATABASE_PASSWORD', '093287858de5eeb54712f700cbb91aa8124d3b202ec0513ef5ed48d9ad7c284b'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
+/****
+connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'vacationdb'),
@@ -9,5 +20,4 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD', '12345678'),
       ssl: env.bool('DATABASE_SSL', false),
     },
-  },
-});
+	***/
