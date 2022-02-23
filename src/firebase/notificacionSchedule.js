81908d1879243ce2d40fb71dc4377868fe743033
mdocarmo <mdocarmo@roshka.com@git config --global user.email mdocarmo@roshka.com>
2022-02-23 15:42:36 -0300

    added new column and schedule on

--- src/firebase/notificacionSchedule.js
+++ src/firebase/notificacionSchedule.js
@@ -1,9 +1,22 @@
 const cron = require("node-cron");
-const { sendNotification} = require("../firebase/firebasePushNotification");
+const { findCustom } = require('../api/workers-info/findworker');
+const { analizeWorker } = require('../logic');
 
-function initSchedule(token, title, body) {
-  cron.schedule("* * * * * *", () => {
-    sendNotification(token, title, body);
+
+function initSchedule() {
+  
+  // At second :00, at minute :00, every hour between 08am and 09am, of every day
+  cron.schedule("0 0 8-9 * *", () => {
+    console.log("entra en lib cron!")
+    findCustom()
+      .then((table) => {
+
+        analizeWorker(table);
+
+      })
+      .catch((error) => {
+        console.log('no pudimo recuperar pa');
+      })
   });
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 'use strict';
 
-const { sendNotification } = require("./firebase/firebasePushNotification");
-const { findCustom } = require('./api/workers-info/findworker');
-const { analizeWorker } = require('./logic');
+const { initSchedule } = require("./firebase/notificacionSchedule");
 
 module.exports = {
 
   register(/*{ strapi }*/) { },
 
   bootstrap(/*{ strapi }*/) {
-    findCustom()
-      .then((table) => {
-
-        analizeWorker(table);
-
-      })
-      .catch((error) => {
-        console.log('no pudimo recuperar pa');
-      })
+    initSchedule()
   },
 };
